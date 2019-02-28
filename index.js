b6318b9cb272a9e811fe120b3ae7cc6011d81cf6
fanon2211 <48079080+fanon2211@users.noreply.github.com>
2019-02-28 12:00:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const fs = require('fs')
 const client = new Discord.Client()
+client.user.setPresence({ game: { name: 'with depression', type: "streaming", url: "https://www.twitch.tv/fanontv"}});
 
 fs.readdir('./events/', (err, files) => {
   files.forEach(file => {
