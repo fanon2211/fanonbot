a382f2de182b0c772a5f5d7526a13f0d22bd0766
fanon2211 <48079080+fanon2211@users.noreply.github.com>
2019-02-28 12:18:29 +0700

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,5 +2,5 @@ module.exports = (client, member) => {
   member.send(`Welcome on the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun :). - FDt`)
   const channel = member.guild.channels.find(ch => ch.name === 'member-log');
   if (!channel) return;
-  channel.send(`Welcome to the Sea Pirate, ${member} !!!`);
+  channel.send(`Welcome to the Sea, Pirate ${member}`);
 }
